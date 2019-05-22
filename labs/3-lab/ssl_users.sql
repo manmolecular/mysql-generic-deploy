6feeab6f02d06820df338644680e8529345ddf0c
manmolecular <regwebghost@yandex.ru>
2019-05-22 17:46:42 +0700

    Done labs

--- labs/3-lab/ssl_users.sql
+++ labs/3-lab/ssl_users.sql
@@ -26,12 +26,22 @@ openssl x509 -in client-cert.pem -issuer
  */
 
 /* 1) admin@'%' - требуется подтверждение владельца и издателя сертификата */
-ALTER USER 'admin'@'%' REQUIRE SSL;
-ALTER USER 'admin'@'%' REQUIRE ISSUER '/CN=MySQL_Server_5.7.25_Auto_Generated_CA_Certificate';
-ALTER USER 'admin'@'%' REQUIRE SUBJECT '/CN=MySQL_Server_5.7.25_Auto_Generated_Client_Certificate';
+ALTER USER 'Admin'@'%' REQUIRE SSL;
+ALTER USER 'Admin'@'%' REQUIRE ISSUER '/CN=MySQL_Server_5.7.25_Auto_Generated_CA_Certificate';
+ALTER USER 'Admin'@'%' REQUIRE SUBJECT '/CN=MySQL_Server_5.7.25_Auto_Generated_Client_Certificate';
 
 /* 2) firewallAdmin@'%' - требуется только защищённое соединение */
+/* CONNECT:
+mysql -P 3306 --protocol=tcp -u firewallAdmin -p --ssl-mode=REQUIRED
+*/
 ALTER USER 'firewallAdmin'@'%' REQUIRE SSL;
 
 /* 3) pwdAnalyst@'%' - требуется подтверждение: использование любого валидного электронного сертификата в рамках инфраструктуры открытых ключей */
 ALTER USER 'pwdAnalyst'@'%' REQUIRE X509;
+
+/* 4) pwdLoader@'%' - требуется подтверждение издателя сертификата, причём издатель должен быть другим нежели чем издатель для сертификата сервера MySQL */
+/* CONNECT : 
+mysql -P 3306 --protocol=tcp -u pwdLoader --ssl-cert=./db_data/data/client-cert.pem --ssl-key=./db_data/data/client-key.pem -p 
+*/
+ALTER USER 'pwdLoader'@'%' REQUIRE SSL;
+ALTER USER 'pwdLoader'@'%' REQUIRE ISSUER '/CN=client';
